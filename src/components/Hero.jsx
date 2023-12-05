18a76706be95549c124bb74c6b19ef3acdc26b9f
enosintech <70450938+enosintech@users.noreply.github.com>
2023-12-05 21:57:15 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -57,7 +57,7 @@ const Hero = () => {
                     <div className='w-1 sm:h-40 h-40 lg:h-40 bg-green'></div>
                 </div>
                 <div className='text-center bg-black'>
-                    <h1 className='font-opensans font-extrabold lg:text-[80px] sm:text-[45px] md:text-[50px] xs:text-[40px] xxxs:text-[28px] text-[35px] lg:leading-[98px] mt-2'>Hi, I am <span className='text-green'>Enos!</span></h1>
+                    <h1 className='font-opensans font-extrabold lg:text-[80px] sm:text-[45px] md:text-[50px] xs:text-[35px] xxxs:text-[28px] text-[35px] lg:leading-[98px] mt-2'>Hi, I am <span className='text-green'>Enos!</span></h1>
                     <p className='font-opensans select-none text-white font-normal xxxs:text-[15px] xs:text-[18px] sm:text-[25px] text-[23px] lg:leading-[40px]'>
                         I develop both Full Stack Web Apps with <span className='text-green'>React.js</span> and Cross platform Mobile Apps with <span className='text-green'>React Native.</span>  I also write confessional poetry and really love fashion.
                     </p>
@@ -69,7 +69,7 @@ const Hero = () => {
             </div>
 
             <div className='flex w-full justify-center'>
-                    <img className='absolute z-20 lg:top-[40%] md:top-[43%] sm:top-[45%] top-[48%] xs:top-[55%] xxs:top-[50%] xxxs:top-[70%] animate-astrobounce mask' src={astro} alt="A picture of an Astronaut"/>
+                    <img className='absolute z-20 lg:top-[40%] md:top-[43%] sm:top-[45%] top-[48%] xs:top-[60%] xxs:top-[50%] xxxs:top-[70%] animate-astrobounce mask' src={astro} alt="A picture of an Astronaut"/>
                     <div className='absolute z-0 lg:top-[30%] md:top-[35%] sm:top-[40%] top-[38%] w-[400px] h-[300px]'></div>
             </div>
 
