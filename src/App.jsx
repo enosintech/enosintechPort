41dfe1322707ffdbfb1a089618a2793d8e527071
enosintech <70450938+enosintech@users.noreply.github.com>
2023-12-05 21:46:05 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Footer from './components/Footer';
 
 const App = () => {
     return <div className='relative z-0 bg-black overflow-hidden'>
-        <div className="w-screen h-screen overflow-scroll">
+        <div className="h-screen overflow-scroll">
             <Navbar />
             <Hero />
             <About /> 
